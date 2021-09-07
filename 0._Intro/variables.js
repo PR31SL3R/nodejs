254f9d0213b8ff65aa2f37b1e3c396baf3222af8
PR31SL3R <Frede5213@github>
2021-09-07 13:32:30 +0200

    Intro excercises done!

--- 0._Intro/variables.js
+++ 0._Intro/variables.js
@@ -27,3 +27,20 @@ console.log(person);
 
 const immuteableperson = Object.freeze(person)
 immuteableperson.name = "new name";
+
+//var
+
+var oldSchoolJS = -10;
+// med var kan man risikere at overskrive variabler da var er "global" og let holder sig til scopet
+
+//for(i = 1; i<6; i++){
+//    console.log(i);
+//}
+
+for (let i = 1; i < 6; i++){
+    setTimeout(function runsafftheloop(){
+        console.log(i)
+    },1000)
+    
+
+}
