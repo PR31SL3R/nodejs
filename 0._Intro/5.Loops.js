353c1c4a7281b8bcc542e9dd28d3b76ea0c11635
PR31SL3R <Frede5213@github>
2021-09-21 14:47:00 +0200

    test

--- 0._Intro/5.Loops.js
+++ 0._Intro/5.Loops.js
@@ -1,19 +1,14 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-
-
-array.forEach(element => {
-    
-});
-
-array.forEach(element => {
-    
-});
-
-for (const iterator of object) {
-    
-}
-
 const array = ["clea","emojo",420,true,"\u2606"]
+
+
+array.forEach(element => {
+    
+  
+});
+
+
+console.log(array)
+
+array.forEach(element => {
+    
+});
