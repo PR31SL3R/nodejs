6628fdcaabcbbf04a47cb8b1a60c0cdbf2b49fa5
PR31SL3R <Frede5213@github>
2021-09-21 15:02:23 +0200

    loops

--- 0._Intro/5.Loops.js
+++ 0._Intro/5.Loops.js
@@ -12,3 +12,32 @@ console.log(array)
 array.forEach(element => {
     
 });
+
+// FOR LOOP BRUGES NÅR DER SKAL TÆLLES FRA EN RANGE
+
+// FOR EACH LOOP HVIS VI IKKE BRUGER DATAEN EFTER
+
+// MAP LOOPER EN TIL EN
+
+// FILTER SORTERER 
+
+// REDUCE KAN MAPPE OG ACCUMMULERE
+
+const favoriteThings = ["Cleo", "🍆", 420, true, "\u2606"];
+
+// write a for each loop
+// favoriteThings.forEach((element, index, array) => console.log(element, index, array));
+// favoriteThings.forEach((favoriteThing) => console.log(favoriteThing));
+// create an array that turns them all to Strings and make them say: "Ooh I like " + favoriteThing
+
+const favoriteThingsWithCommentary = [];
+favoriteThings.forEach(favoriteThing => favoriteThingsWithCommentary.push(`Ooh I like ${ favoriteThing }`));
+
+const subjectiveFavoriteThings = favoriteThings.map(favoriteThing => `Ooh I like ${ favoriteThing }`);
+// console.log(subjectiveFavoriteThings);
+
+// console.log(favoriteThings.indexOf("🍆"));
+
+const longFavoriteThings = favoriteThings.filter(favoriteThing => favoriteThing.length > 1);
+console.log(longFavoriteThings);
+
