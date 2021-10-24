815978a9d2d3d7afc362d9da130b20f395fc0b64
PR31SL3R <Frede5213@github>
2021-10-24 16:17:55 +0200

    mandatory_1

--- 7._Mandatory_1/app.js
+++ 7._Mandatory_1/app.js
@@ -1,16 +1,57 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/cleo/cleo.html`);
+app.use(express.static('public'))
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index/index.html");
 });
 
-const PORT = process.env.PORT || 8080;
+app.get("/week1", (req, res) => {
+    res.sendFile(__dirname + "/public/week1.html");
+});
 
-// eslint-disable-next-line no-unused-vars
-app.listen(PORT, (_error) => {
-  // eslint-disable-next-line no-console
-  console.log('server is running on port', PORT);
+app.get("/week2", (req, res) => {
+    res.sendFile(__dirname + "/public/week2.html");
+});
+
+app.get("/week3", (req, res) => {
+    res.sendFile(__dirname + "/public/week3.html");
+});
+
+app.get("/week4", (req, res) => {
+    res.sendFile(__dirname + "/public/week4.html");
+});
+
+app.get("/week5", (req, res) => {
+    res.sendFile(__dirname + "/public/week5.html");
+});
+app.get("/week6", (req, res) => {
+    res.sendFile(__dirname + "/public/week6.html");
+});
+app.get("/week7", (req, res) => {
+    res.sendFile(__dirname + "/public/week7.html");
+});
+app.get("/week8", (req, res) => {
+    res.sendFile(__dirname + "/public/week8.html");
+});
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", Number(port));
 });
--- 7._Mandatory_1/public/cleo/cleo.js
+++
@@ -1,9 +0,0 @@
-console.log('meow meow');
-
-fetch('https://www.boredapi.com/api/activity')
-  .then((response) => response.json())
-  .then((result) => {
-    const catFactsDiv = document.getElementById('catfacts');
-    catFactsDiv.innerText = result.activity;
-    
-  });
