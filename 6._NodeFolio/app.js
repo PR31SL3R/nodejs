d7dd59f234a267dac082252ab78ac5f08eba68f7
PR31SL3R <Frede5213@github>
2021-10-26 13:53:17 +0200

    nodefolio

--- 6._NodeFolio/app.js
+++ 6._NodeFolio/app.js
@@ -1,16 +1,22 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/frontpage/frontpage.html`);
+app.use(express.static("public"));
+
+/* Import and use routes */
+const projectsRouter = require("./routers/projects.js");
+app.use(projectsRouter.router);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
+});
+
+app.get("/projects", (req, res) => {
+    res.sendFile(__dirname + "/public/projects/projects.html");
 });
 
 const PORT = process.env.PORT || 8080;
 
-// eslint-disable-next-line no-unused-vars
-app.listen(PORT, (_error) => {
-  // eslint-disable-next-line no-console
-  console.log('server is running on port', PORT);
+app.listen(PORT, (error) => {
+    console.log("Server is running on", PORT);
 });
